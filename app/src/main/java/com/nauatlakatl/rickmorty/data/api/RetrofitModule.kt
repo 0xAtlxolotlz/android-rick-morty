6a68dbbcfeb233b8c7d51efdce3ed5c4fe2f52a3
AlbertSawZ <albertsawz@gmail.com>
2023-05-31 17:18:36 -0600

    build: add RetrofitModule

---
+++ app/src/main/java/com/nauatlakatl/rickmorty/data/api/RetrofitModule.kt
@@ -0,0 +1,26 @@
+package com.nauatlakatl.rickmorty.data.api
+
+import com.nauatlakatl.rickmorty.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().apply {
+            addConverterFactory(GsonConverterFactory.create())
+            client(okHttpClient)
+            baseUrl(BuildConfig.API_BASE_URL)
+        }.build()
+    }
+}

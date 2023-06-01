ebbae8fcc6946a2714dd92a7553309e632e27d78
AlbertSawZ <albertsawz@gmail.com>
2023-05-31 19:44:38 -0600

    bugfix: set RickMortyNavGraph as the content

--- app/src/main/java/com/nauatlakatl/rickmorty/MainActivity.kt
+++ app/src/main/java/com/nauatlakatl/rickmorty/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.nauatlakatl.rickmorty.ui.theme.RickAndMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RickAndMortyTheme {
-
+                RickMortyNavGraph()
             }
         }
     }

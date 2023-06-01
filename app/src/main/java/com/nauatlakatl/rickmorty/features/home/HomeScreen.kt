744208d8f2494cd5d79b272c5a6106a43ef4e5a5
AlbertSawZ <albertsawz@gmail.com>
2023-05-31 22:09:21 -0600

    feat: introduce CharacterCard composable

--- app/src/main/java/com/nauatlakatl/rickmorty/features/home/HomeScreen.kt
+++ app/src/main/java/com/nauatlakatl/rickmorty/features/home/HomeScreen.kt
@@ -1,9 +1,17 @@
 package com.nauatlakatl.rickmorty.features.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,7 +48,62 @@ fun HomeScreen(
             contentPadding = paddingValues
         ) {
             items(characters) { character ->
-                Text(text = character.name)
+                with(character) {
+                    CharacterCard(name = name, species = species, image = image)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CharacterCard(
+    name: String,
+    species: String,
+    image: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .aspectRatio(1F)
+            .padding(4.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            AsyncImage(
+                modifier = modifier.fillMaxSize(),
+                model = image,
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ), startY = 0F
+                        )
+                    )
+            ) {
+                Text(
+                    text = name,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = modifier.padding(horizontal = 2.dp),
+                    maxLines = 1
+                )
+                Text(
+                    text = species,
+                    color = Color.White,
+                    modifier = modifier.padding(start = 2.dp, end = 2.dp, bottom = 2.dp)
+                )
             }
         }
     }

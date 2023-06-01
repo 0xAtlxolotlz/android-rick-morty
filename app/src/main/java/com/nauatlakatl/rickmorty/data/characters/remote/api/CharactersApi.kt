5248c79814261e5d35c1590c27171777e4da2a15
AlbertSawZ <albertsawz@gmail.com>
2023-05-31 21:08:01 -0600

    bugfix: remove trailing slash from path

--- app/src/main/java/com/nauatlakatl/rickmorty/data/characters/remote/api/CharactersApi.kt
+++ app/src/main/java/com/nauatlakatl/rickmorty/data/characters/remote/api/CharactersApi.kt
@@ -7,6 +7,6 @@ import retrofit2.http.GET
 
 interface CharactersApi {
 
-    @GET("/character")
+    @GET("character")
     suspend fun getAllCharacters(): Response<ResponseListWrapper<CharactersResponse>>
 }

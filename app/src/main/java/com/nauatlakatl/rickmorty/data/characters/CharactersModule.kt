3e4a02b9606749661873dd6ceef211bafbf373ec
AlbertSawZ <albertsawz@gmail.com>
2023-05-31 18:45:47 -0600

    bugfix: add missing annotations to provide CharactersApi in CharactersModule

--- app/src/main/java/com/nauatlakatl/rickmorty/data/characters/CharactersModule.kt
+++ app/src/main/java/com/nauatlakatl/rickmorty/data/characters/CharactersModule.kt
@@ -3,14 +3,18 @@ package com.nauatlakatl.rickmorty.data.characters
 import com.nauatlakatl.rickmorty.data.characters.remote.api.CharactersApi
 import com.nauatlakatl.rickmorty.data.common.module.RetrofitModule
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module(includes = [RetrofitModule::class])
 @InstallIn(SingletonComponent::class)
 class CharactersModule {
 
+    @Singleton
+    @Provides
     fun provideCharactersApi(retrofit: Retrofit): CharactersApi {
         return retrofit.create(CharactersApi::class.java)
     }

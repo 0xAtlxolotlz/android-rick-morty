6393466165358565f824d11aa8ac8a500db62cc7
AlbertSawZ <albertsawz@gmail.com>
2023-06-01 06:47:46 -0600

    feat: add TopAppBar in HomeScreen

--- app/src/main/java/com/nauatlakatl/rickmorty/features/home/HomeScreen.kt
+++ app/src/main/java/com/nauatlakatl/rickmorty/features/home/HomeScreen.kt
@@ -16,8 +16,13 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -27,10 +32,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.nauatlakatl.rickmorty.R
 import com.nauatlakatl.rickmorty.domain.characters.entity.CharactersEntity
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -46,7 +54,24 @@ fun HomeScreen(
         homeViewModel.fetchCharacters()
     }
 
-    Scaffold(modifier = modifier.fillMaxSize()) { paddingValues ->
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_name)) },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            painterResource(R.drawable.ic_filter),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+            )
+        }
+    ) { paddingValues ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             modifier = modifier.fillMaxSize(),
